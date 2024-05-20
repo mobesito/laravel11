0a14393b6debbe0f4fe864bb603a8ebb52087afe
mobesito <edwin54_ayala@hotmail.com>
2024-05-19 19:53:59 -0600

    avance laravel 11

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <x-slot:title>
+        About
+    </x-slot:title>
+    <x-slot:heading>
+        About page
+    </x-slot:heading>
+    <h1>Hello from the about page</h1>
+</x-layout>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <x-slot:title>
+        Contact
+    </x-slot:title>
+    <x-slot:heading>
+        Contact page
+    </x-slot:heading>
+    <h1>Hello from the contact page</h1>
+</x-layout>
